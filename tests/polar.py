5279f139b0520e47c6998a2652a5071e6465bf73
Enrico Ferreguti <enricofer@gmail.com>
2021-04-01 10:31:40 +0200

    polar tests failure

--- tests/polar.py
+++ tests/polar.py
@@ -1,5 +1,10 @@
 import unittest
 import weatherrouting
+import os
 
 class TestPolar(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.polar_obj = weatherrouting.Polar(os.path.join(os.path.dirname(__file__),'Bavaria38.pol'))
+
+    def test_pointDistance(self):
+        self.assertEqual(self.polar_obj.getSpeed(8,60),6.48, 0.01)  
